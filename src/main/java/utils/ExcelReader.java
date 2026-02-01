58cde47d939ab00436426aa89b082a47a6e166c6
pop <230184@astanait.edu.kz>
2026-02-01 18:10:26 +0500

    ыыыы

---
+++ src/main/java/utils/ExcelReader.java
@@ -0,0 +1,32 @@
+package utils;
+
+import org.apache.poi.ss.usermodel.*;
+import java.io.File;
+import java.io.FileInputStream;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ExcelReader {
+
+    public static List<String[]> readExcel(String path) {
+        List<String[]> data = new ArrayList<>();
+
+        try {
+            Workbook workbook = WorkbookFactory.create(new FileInputStream(new File(path)));
+            Sheet sheet = workbook.getSheetAt(0);
+
+            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+                Row row = sheet.getRow(i);
+                data.add(new String[]{
+                        row.getCell(0).getStringCellValue(),
+                        row.getCell(1).getStringCellValue(),
+                        row.getCell(2).getStringCellValue(),
+                        row.getCell(3).getStringCellValue()
+                });
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+}
